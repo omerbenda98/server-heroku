5bd4c63895def1bd5a7a538e3c8153b1dfefaa81
omerbenda98 <omerbenda98@gmail.com>
2024-08-27 17:52:15 +0300

    cors change

--- app.js
+++ app.js
@@ -35,9 +35,28 @@ module.exports = function (app) {
       },
       app
     );
+    const allowedOrigins = [
+      "https://omerbenda98.github.io",
+      "http://localhost:3000",
+    ];
 
+    app.use(
+      cors({
+        origin: function (origin, callback) {
+          // allow requests with no origin
+          // (like mobile apps or curl requests)
+          if (!origin) return callback(null, true);
+          if (allowedOrigins.indexOf(origin) === -1) {
+            var msg =
+              "The CORS policy for this site does not " +
+              "allow access from the specified Origin.";
+            return callback(new Error(msg), false);
+          }
+          return callback(null, true);
+        },
+      })
+    );
     // Middleware
-    app.use(cors({ origin: "https://omerbenda98.github.io" }));
     app.use(morgan(chalk.cyan(":method :url :status :response-time ms")));
     app.use(express.json());
     app.use(express.static("public"));
